b2a5e97a1199f2907f1c6e31e1763cca15060480
John Emmanuel Montemar <montemarjohn66@gmail.com>
2019-10-05 20:11:31 +0800

    jadzo created

---
+++ components/footer.php
@@ -0,0 +1,25 @@
+<footer>
+    <div class="row">
+        <div class="col-sm-4">
+            <img src="img/jadzo_logo.jpg" class="footerImg">
+        </div>
+        <div class="col-sm-4">
+            <div>
+                <h2>Visit Us</h2>
+                <div style="width:100%;">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7841.561961139413!2d122.32493174276588!3d10.674101313052219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33ae599d7c050997%3A0xd6055afe8d92530e!2sJADZO%20CUSTOMS!5e0!3m2!1sen!2sph!4v1569997917828!5m2!1sen!2sph" style="width:100%;" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="info">
+                <h2>Call Us</h2>
+                <p><i class="fa fa-phone"></i>(+63)912 345 6789</p>
+                <a href="https://www.facebook.com/pg/dekulorprints/photos/?tab=album&album_id=1574953726146143">
+                    <i class="fa fa-facebook"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</footer>
